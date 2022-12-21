af9a358210e6c664c4113a7f05aa2ebe43754947
kshyun28 <jasper.d.gabriel@gmail.com>
2022-12-21 23:20:03 +0800

    test: fix unit tests

--- src/utils/jsonResponse.test.ts
+++ src/utils/jsonResponse.test.ts
@@ -1,13 +1,24 @@
 import * as jsonResponse from './jsonResponse';
 
 describe('Unit test for utils: jsonResponse', function () {
+    const headers = {
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Allow-Origin': '*',
+    }
     it('generates successful response', () => {
-        const result = jsonResponse.success(null, 'success');
-        expect(result.message).toEqual('success'), expect(result.copyright).toEqual('Copyright 2022, P33R');
+        const result = jsonResponse.generateResponse(200, headers, 'success', null);
+        const body = JSON.parse(result.body)
+        expect(result.statusCode).toEqual(200);
+        expect(result.headers).toEqual(headers);
+        expect(body.message).toEqual('success');
     });
 
     it('generates failing response', () => {
-        const result = jsonResponse.fail(null, 'fail');
-        expect(result.message).toEqual('fail'), expect(result.copyright).toEqual('Copyright 2022, P33R');
+        const result = jsonResponse.generateResponse(400, headers, 'fail', null);
+        const body = JSON.parse(result.body)
+        expect(result.statusCode).toEqual(400);
+        expect(result.headers).toEqual(headers);
+        expect(body.message).toEqual('fail');
     });
 });
